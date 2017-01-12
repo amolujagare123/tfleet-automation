d603213f28007ce803da9b6f02626020b78b11bc
SAI RAM <Priyanka9320@gmail.com>
2017-01-12 14:19:59 +0530

    Billing pages
    addClientUserTest ,addFleetUserTest,Permission,FleetAverage
    QuickInvoice

--- testsrc/regression/QuickInvoice/QuickInvoiceTest.java
+++ testsrc/regression/QuickInvoice/QuickInvoiceTest.java
@@ -9,12 +9,24 @@ import com.tfleet.pages.Menu;
 import com.tfleet.pages.QuickInvoice.QuickInvoice;
 import com.tfleet.utilities.Driver;
 import com.tfleet.utilities.initExtentReport;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
+import static com.tfleet.utilities.TakeScreenshot.takeScreenshot;
 
 
 /**
@@ -41,7 +53,8 @@ public class QuickInvoiceTest {
                                  String dateIn, String totalDays, String timeOut, String timeIn, String totalHrs, String kmOut,
                                  String kmIn, String totalKm, String packageName, String feedBack, String basicRate, String extraKmAmount,
                                  String extraHrAmount, String outStationAllowence, String nightAllowence, String parking, String toll,
-                                 String otherRecoverable, String interStateTax, String total, String serviceTaxAmount, String grossAmount)
+                                 String otherRecoverable, String interStateTax, String total, String serviceTaxAmount, String grossAmount,
+                                 String expected)
             throws IOException {
 
         ExtentTest test = extent.startTest("Test Add company | save record", "To test the save button functionality");
@@ -71,58 +84,288 @@ public class QuickInvoiceTest {
             test.log(LogStatus.INFO, "Rental City Select");
 
             quickInvoice.setTxtNameOfGuest(nameOfGuest);
-            test.log(LogStatus.INFO,"Name of guest set");
+            test.log(LogStatus.INFO, "Name of guest set");
 
             quickInvoice.setTxtBookedBy(bookedBy);
-            test.log(LogStatus.INFO,"Booked by set");
+            test.log(LogStatus.INFO, "Booked by set");
 
             quickInvoice.setTxtMobileno(mobileNo);
-            test.log(LogStatus.INFO,"Mobile no set");
+            test.log(LogStatus.INFO, "Mobile no set");
 
             quickInvoice.setTxtEmail(email);
-            test.log(LogStatus.INFO,"Email set");
+            test.log(LogStatus.INFO, "Email set");
 
             quickInvoice.clickOwned();
-            test.log(LogStatus.INFO,"Clicked Owned");
+            test.log(LogStatus.INFO, "Clicked Owned");
 
             quickInvoice.setSelectFleetCategory(fleetCategory);
-            test.log(LogStatus.INFO,"Fleet Category select");
+            test.log(LogStatus.INFO, "Fleet Category select");
 
             quickInvoice.setSelectFleetType(fleetType);
-            test.log(LogStatus.INFO,"Fleet Type select");
+            test.log(LogStatus.INFO, "Fleet Type select");
 
             quickInvoice.setSelectFleetNo(fleetNumber);
-            test.log(LogStatus.INFO,"Fleet No select");
+            test.log(LogStatus.INFO, "Fleet No select");
 
             quickInvoice.setSelectChauffeurName(chauffeurName);
-            test.log(LogStatus.INFO,"Chauffeur Name select");
+            test.log(LogStatus.INFO, "Chauffeur Name select");
 
             quickInvoice.setSelectTimeOutHr(timeOut);
-            test.log(LogStatus.INFO,"TimeOutHr Select");
+            test.log(LogStatus.INFO, "TimeOutHr Select");
 
             quickInvoice.setSelectTimeOutMin(timeOut);
-            test.log(LogStatus.INFO,"TimeOutMin Select");
+            test.log(LogStatus.INFO, "TimeOutMin Select");
+
+            quickInvoice.setSelectTimeInHr(timeIn);
+            test.log(LogStatus.INFO,"TimeinHr select ");
+
+            quickInvoice.setSelectTimeInMin(timeIn);
+            test.log(LogStatus.INFO,"TimeinMin select");
 
             quickInvoice.setTotalHrs(totalHrs);
-            test.log(LogStatus.INFO,"Total Hrs set");
+            test.log(LogStatus.INFO, "Total Hrs set");
 
             quickInvoice.setTxtKmOut(kmOut);
-            test.log(LogStatus.INFO,"KM out set");
+            test.log(LogStatus.INFO, "KM out set");
 
             quickInvoice.setTxtKmIn(kmIn);
-            test.log(LogStatus.INFO,"Total KmIn set");
+            test.log(LogStatus.INFO, "KmIn set");
+
+            quickInvoice.setTotalKm(totalKm);
+            test.log(LogStatus.INFO,"Total KM set");
 
             quickInvoice.setSelectPackageName(packageName);
-            test.log(LogStatus.INFO,"Package Name Select");
+            test.log(LogStatus.INFO, "Package Name Select");
 
             quickInvoice.setTxtFeedBack(feedBack);
-            test.log(LogStatus.INFO,"FeedBack set");
+            test.log(LogStatus.INFO, "FeedBack set");
+
+            quickInvoice.setTxtBasicRate((basicRate));
+            test.log(LogStatus.INFO, "Basic Rate set");
+
+            quickInvoice.setTxtExtraKmAmount(extraKmAmount);
+            test.log(LogStatus.INFO, "Extra KM Amount set");
+
+            quickInvoice.setTxtExtraHrAmount(extraHrAmount);
+            test.log(LogStatus.INFO, "Extra HR Amount set");
+
+            quickInvoice.setTxtOutStationAllowance(outStationAllowence);
+            test.log(LogStatus.INFO, "Out Station Allowance set");
+
+            quickInvoice.setTxtNightAllowance(nightAllowence);
+            test.log(LogStatus.INFO, "Night Station Allowance set");
+
+            quickInvoice.setTxtParking(parking);
+            test.log(LogStatus.INFO, "Parking Allowance set");
+
+            quickInvoice.setTxtToll(toll);
+            test.log(LogStatus.INFO, "Toll set");
+
+            quickInvoice.setTxtOtherRecovery(otherRecoverable);
+            test.log(LogStatus.INFO, "Other Recovereable set");
+
+            quickInvoice.setTxtInterStateTax(interStateTax);
+            test.log(LogStatus.INFO, "InterStateTax set");
+
+            quickInvoice.clickchkServiceTax();
+            test.log(LogStatus.INFO, "Service tax checked");
+
+            quickInvoice.setTotal(total);
+            test.log(LogStatus.INFO, "Total set");
+
+            quickInvoice.setTxtServiceTaxAmount(serviceTaxAmount);
+            test.log(LogStatus.INFO, "service Tax Amount set");
+
+            quickInvoice.setTxtGrossAmount(grossAmount);
+            test.log(LogStatus.INFO, "Gross Amount set");
+
+            quickInvoice.clickchkSendEmailToClient();
+            test.log(LogStatus.INFO, "Send email to client Checked");
+
+            quickInvoice.clickCreate();
+            test.log(LogStatus.INFO, "Create clicked");
 
 
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            Alert alert = driver.switchTo().alert();
 
+
+            String actual = alert.getText();
+
+            alert.accept();
+            test.log(LogStatus.INFO, "alert displayed as " + actual);
+
+
+            Assert.assertEquals(actual.trim(), expected.trim());
+            test.log(LogStatus.PASS, "Company Added Successfully");
+
+            test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture("./screenshots/" + takeScreenshot(driver)));
 
 
         } catch (AssertionError e) {
+            test.log(LogStatus.FAIL, e);
+            test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture("./screenshots/" + takeScreenshot(driver)));
+
+        } catch (NoSuchElementException e) {
+            test.log(LogStatus.FAIL, "Element not found : " + e);
+            test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture("./screenshots/" + takeScreenshot(driver)));
+
+        } catch (Throwable e) {
+            test.log(LogStatus.ERROR, "There is Error : " + e);
+            test.log(LogStatus.INFO, "Snapshot below: " + test.addScreenCapture("./screenshots/" + takeScreenshot(driver)));
         }
+
+        extent.endTest(test);
+        extent.flush();
     }
-}
+    @DataProvider
+    public Object[][] getdata() throws IOException
+    {
+        FileInputStream fileInputStream=new FileInputStream("Excelsheet/Regression_Master.xls");
+
+        HSSFWorkbook workbook=new HSSFWorkbook(fileInputStream);
+
+        HSSFSheet worksheet=workbook.getSheet("AddCompanyData");
+        int rowCount=worksheet.getPhysicalNumberOfRows();
+        String[][] data = new String[rowCount-1][40];
+        for(int i=1;i<rowCount;i++) {
+            HSSFRow row = worksheet.getRow(i);
+
+            HSSFCell previousInvoiceCell = row.getCell(0);
+            if (previousInvoiceCell == null) {
+                data[i - 1][0] = "";
+            } else {
+                previousInvoiceCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][0] = previousInvoiceCell.getStringCellValue();
+            }
+
+            HSSFCell bookingNumberCell = row.getCell(1);
+            if (bookingNumberCell == null) {
+                data[i - 1][1] = "";
+            } else {
+                bookingNumberCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][1] = bookingNumberCell.getStringCellValue();
+            }
+
+            HSSFCell CompanyCell = row.getCell(2);
+            if (CompanyCell == null) {
+                data[i - 1][2] = "";
+            } else {
+                CompanyCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][2] = CompanyCell.getStringCellValue();
+            }
+
+            HSSFCell reportingAddressCell = row.getCell(3);
+            if (reportingAddressCell == null) {
+                data[i - 1][3] = "";
+            } else {
+                reportingAddressCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][3] = reportingAddressCell.getStringCellValue();
+            }
+
+            HSSFCell rentalCityCell = row.getCell(4);
+            if (rentalCityCell == null) {
+                data[i - 1][4] = "";
+            } else {
+                rentalCityCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][4] = rentalCityCell.getStringCellValue();
+            }
+
+            HSSFCell nameOfGuestCell = row.getCell(5);
+            if (nameOfGuestCell == null) {
+                data[i - 1][5] = "";
+            } else {
+                nameOfGuestCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][5] = nameOfGuestCell.getStringCellValue();
+            }
+
+            HSSFCell bookedByCell = row.getCell(6);
+            if (bookedByCell == null) {
+                data[i - 1][6] = "";
+            } else {
+                bookedByCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][6] = bookedByCell.getStringCellValue();
+            }
+
+            HSSFCell mobileNoCell = row.getCell(7);
+            if (mobileNoCell == null) {
+                data[i - 1][7] = "";
+            } else {
+                mobileNoCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][7] = mobileNoCell.getStringCellValue();
+            }
+
+            HSSFCell emailCell = row.getCell(8);
+            if (emailCell == null) {
+                data[i - 1][8] = "";
+            } else {
+                emailCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][8] = emailCell.getStringCellValue();
+            }
+
+            HSSFCell fleetCategoryCell = row.getCell(9);
+            if (fleetCategoryCell == null) {
+                data[i - 1][9] = "";
+            } else {
+                fleetCategoryCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][9] = fleetCategoryCell.getStringCellValue();
+            }
+
+            HSSFCell fleetTypeCell = row.getCell(10);
+            if (fleetTypeCell == null) {
+                data[i - 1][10] = "";
+            } else {
+                fleetTypeCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][10] = fleetTypeCell.getStringCellValue();
+            }
+
+            HSSFCell fleetNumberCell = row.getCell(11);
+            if (fleetNumberCell == null) {
+                data[i - 1][11] = "";
+            } else {
+                fleetNumberCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][11] = fleetNumberCell.getStringCellValue();
+            }
+
+            HSSFCell chauffeurNameCell = row.getCell(11);
+            if (chauffeurNameCell == null) {
+                data[i - 1][11] = "";
+            } else {
+                chauffeurNameCell.setCellType(Cell.CELL_TYPE_STRING);
+                data[i - 1][11] = chauffeurNameCell.getStringCellValue();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
